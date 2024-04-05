e60f1b467c4980bb9e34224ca7c8f9d6e05eee1e
KayongoYongo <sknsammie10@gmail.com>
2024-04-05 20:54:54 +0300

    added telegram bot

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,47 +1,22 @@
 import os
 from dotenv import load_dotenv
-from telegram.ext import Updater, CommandHandler
+import telebot
 
 # Load environment variables from .env file
 load_dotenv()
 
 # Access the variables
-api_token = os.getenv("API_TOKEN")
+BOT_TOKEN = os.getenv("API_TOKEN")
 username = os.getenv("USERNAME")
 
+bot = telebot.TeleBot(BOT_TOKEN)
 
-def start(update, context):
-    """
-    Handle the /start command.
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, "Wassup, how are you doing?")
 
-    This function is called when the user starts a conversation with the bot by sending the /start command.
-    It replies with a greeting message to welcome the user to the Greeting Bot.
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
 
-    Parameters:
-        update (telegram.Update): The update object containing information about the incoming message.
-        context (telegram.ext.CallbackContext): The context object containing additional information and functionality for handling the update.
-
-    Returns:
-        None
-    """
-    update.message.reply_text('Hello! Welcome to the Tutorial Bot. Hope it works')
-
-
-def main():
-    # Create the Updater and pass it your bot's token
-    updater = Updater(api_token, use_context=True)
-
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-
-    # Register the /start handler
-    dp.add_handler(CommandHandler("start", start))
-
-    # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+bot.infinity_polling()
