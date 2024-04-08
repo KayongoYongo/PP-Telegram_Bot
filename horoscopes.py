1cf2c28c7d6ec5835c750285ebe0b34b7bac714a
KayongoYongo <sknsammie10@gmail.com>
2024-04-08 19:21:08 +0300

    a script that acquires information about horoscopes

---
+++ horoscopes.py
@@ -0,0 +1,18 @@
+import requests
+
+# This function will fetch horoscope data for a particular day
+def get_daily_horoscope(sign: str, day: str) -> dict:
+    """Get daily horoscope for a zodiac sign.
+
+    Arguments:
+        sign:str - Zodiac sign
+        day:str - Date in format (YYYY-MM-DD) OR TODAY OR TOMORROW OR YESTERDAY
+    
+    Return:
+        dict - JSON data
+    """
+    url = "https://horoscope-app-api.vercel.app/api/v1/get-horoscope/daily"
+    params = {"sign": sign, "day": day}
+    response = requests.get(url, params)
+
+    return response.json()
